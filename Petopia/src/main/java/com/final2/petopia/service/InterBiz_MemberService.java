04d4907a0caf63833ad4998b5e7820d460f39cdf
leehyeon12 <leehyun6691@gmail.com>
2019-01-25 22:27:41 +0900

    [19-01-25] merge_고은 + 혜원 + 수미 + 지석

--- Petopia/src/main/java/com/final2/petopia/service/InterBiz_MemberService.java
+++ Petopia/src/main/java/com/final2/petopia/service/InterBiz_MemberService.java
@@ -1,7 +1,26 @@
 package com.final2.petopia.service;
 
+import java.util.HashMap;
+import java.util.List;
+
+import com.final2.petopia.model.Biz_MemberVO;
+import com.final2.petopia.model.MemberVO;
+
 public interface InterBiz_MemberService {
 
-	int idDuplicateCheck(String userid);
+	// 태그 리스트 보여주기
+	List<HashMap<String, String>> selectHaveTagList();
+	
+	// 아이디 중복체크
+	int selectBizMemberIdIsUsed(String userid);
+	
+	// 태그가 있는 경우 회원가입
+	int insertMemberByMvoTagList(Biz_MemberVO mvo, String[] tagNoArr, String[] tagNameArr);
+
+	// 태그가 없는 경우 회원가입
+	int insertMemberByMvo(Biz_MemberVO mvo);
+
+
+
 
 }
