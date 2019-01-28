47748deffb6ebc478e5122c70e13a27df14efb83
choi-minju <minju3667@naver.com>
2019-01-28 18:10:35 +0900

    190128_나의 갔다온 병원 리스트 하는중(+리뷰)...

---
+++ Petopia/src/main/java/com/final2/petopia/service/InterReviewService.java
@@ -0,0 +1,18 @@
+package com.final2.petopia.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface InterReviewService {
+
+	// === 2019.01.28 ==== //
+	// *** 리뷰 쓸 병원 리스트 조회 *** //
+	int selectTotalCount(HashMap<String, Integer> paramap); // 병원 리스트 카운트 - 기간이 없는
+	int selectTotalCountByPeriod(HashMap<String, Integer> paramap); // 병원 리스트 카운트 - 기간이 있는
+	
+	List<HashMap<String, String>> selectHosList(HashMap<String, Integer> paramap); // 병원 리스트 - 기간이 없는
+	List<HashMap<String, String>> selectHosListByPeriod(HashMap<String, Integer> paramap); // 병원 리스트 - 기간이 있는
+	
+	// === 2019.01.28 ==== //
+	
+}
