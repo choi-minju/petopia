6a184458a949601a2e86d408f00516ec5601c825
jiminy <knowledge0223@gmail.com>
2019-01-30 17:47:26 +0900

    190130_commitandpush_jimin

--- Petopia/src/main/java/com/final2/petopia/model/InterNotificationDAO.java
+++ Petopia/src/main/java/com/final2/petopia/model/InterNotificationDAO.java
@@ -1,6 +1,6 @@
-package com.final2.petopia.service;
+package com.final2.petopia.model;
 
-public interface InterNotificationService {
+public interface InterNotificationDAO {
 
 	// 회원의 고유번호를 이용한 안읽은 알림 갯수 나타내기
 	String selectUnreadNotificationCount(int idx);
